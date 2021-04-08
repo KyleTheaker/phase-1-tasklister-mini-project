222e46d463a9bca7e42577202b929f58615af7c2
Kyle <kyle.theaker@yahoo.com>
2021-04-08 16:51:50 -0700

    Zach Frohardt, Kyle Theaker, Yayo, group project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.querySelector('#create-task-form').addEventListener('submit', submitButtonHandler)
+console.log(document.querySelector('#create-task-form'))
+  
+  
 });
+function submitButtonHandler(event) {
+  event.preventDefault()
+  console.log('teachme')
+
+  const tasks = document.querySelector('#tasks')
+  const newElement = document.createElement('li')
+  newElement.innerText = document.querySelector('#new-task-description').value
+  let button = document.createElement('button')
+  button.innerText = 'X'
+
+  newElement.appendChild(button)
+  
+  button.addEventListener('click', event => {
+    event.preventDefault();
+    console.dir(event.target)
+    event.target.closest('li').remove()
+  })
+  
+
+  
+  tasks.appendChild(newElement)
+  
+}
+
